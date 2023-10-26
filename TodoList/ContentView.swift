e7a23ed5a7e16357b6638f634c1a99dbeb5bfed8
Elaidzha1940 <eli080293@gmail.com>
2023-10-26 19:01:17 +0300

    TodoListApp

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: TodoList
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+         
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: TodoList
+//  File: TodoListApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.10.2023
+//
+//  */
+
+import SwiftUI
+
+/*
+ MVVM Architecture
+ 
+ Model - data point
+ View - UI
+ ViewModel - manages Models for View
+ 
+ */
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
